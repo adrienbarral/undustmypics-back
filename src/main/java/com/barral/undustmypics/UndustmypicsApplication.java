5040737dba55e47839d6b4e58a46fc6c03716e7e
Adrien BARRAL <aba@robopec.com>
2017-12-09 14:44:06 +0100

    Dépot initial.

---
+++ src/main/java/com/barral/undustmypics/UndustmypicsApplication.java
@@ -0,0 +1,12 @@
+package com.barral.undustmypics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UndustmypicsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UndustmypicsApplication.class, args);
+	}
+}
