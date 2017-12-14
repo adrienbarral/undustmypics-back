e0cde571546d522f993bf10b0ad9ccd3e8c80787
Adrien BARRAL <aba@robopec.com>
2017-12-14 20:34:49 +0100

    Le undusting fonctionne via l'appel REST.

--- src/main/java/com/barral/controllers/PictureUndustingController.java
+++ src/main/java/com/barral/controllers/PictureUndustingController.java
@@ -1,5 +1,6 @@
-package com.barral.controlers;
+package com.barral.controllers;
 
+import org.apache.commons.io.IOUtils;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
@@ -19,7 +20,7 @@ public class PictureUndustingController {
     @ResponseBody
     byte[] undustPicture(@RequestParam("file") MultipartFile file) throws IOException {
         if (file.isEmpty()) return new byte[]{};
-        File outFile = new File(tmpFolder + "/" + file.getName());
+        File outFile = new File(tmpFolder + "/" + file.getOriginalFilename());
         BufferedOutputStream stream =
                 new BufferedOutputStream(new FileOutputStream(outFile));
         stream.write(file.getBytes());
@@ -28,8 +29,6 @@ public class PictureUndustingController {
         Runtime r = Runtime.getRuntime();
         Process p = r.exec(undustExe + " --file " + outFile.getAbsolutePath() +
                 " --seuilRouge 10 --plusGrosseTache 0.07");
-        byte[] resFile = new byte[]{};
-        int byteCount = p.getInputStream().read(resFile);
         try {
             p.waitFor();
         } catch (InterruptedException e) {
@@ -37,11 +36,8 @@ public class PictureUndustingController {
         }
 
 
-        File inFile = new File(new String(resFile));
+        File inFile = new File(tmpFolder + "/UNDUSTED_" + file.getOriginalFilename());
         BufferedInputStream inStream = new BufferedInputStream((new FileInputStream(inFile)));
-        byte[] res = new byte[]{};
-        inStream.read(res);
-
-        return res;
+        return IOUtils.toByteArray(inStream);
     }
 }
--- src/main/java/com/barral/undustmypics/UndustmypicsApplication.java
+++ src/main/java/com/barral/undustmypics/UndustmypicsApplication.java
@@ -2,8 +2,10 @@ package com.barral.undustmypics;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = {"com.barral.controllers"})
 public class UndustmypicsApplication {
 
 	public static void main(String[] args) {
